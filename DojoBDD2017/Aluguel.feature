793b4f0fdb21cbb5fcd9c8588ea1b3feb4b3cc6c
danilo.ruziska <danilo.ruziska@agilecontent.com>
2017-09-04 09:42:43 -0300

    Primeiro commit

---
+++ DojoBDD2017/Aluguel.feature
@@ -0,0 +1,43 @@
+﻿#language:pt-br
+
+Funcionalidade: Aluguel
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+
+Cenário: Aluguel filme
+	Dado que esteja na página inicial
+	E Fechado o Walkthrough
+	E Login efetuado
+	E Filme pesquisado
+	E Filme selecionado
+	Quando Clicado no botão alugar da modal
+	E Pin inserido corretamente 
+	E Clicar no botão de confirmação
+	Então Usuário é encaminhado à tela de reprodução.
+
+
+#	Cenário: Aluguel filme PIN errado
+#	Dado o navegador esta aberto
+#	E a URL acessada é o NOW
+#	E Fechado o Walkthrough
+#	E Login efetuado
+#	E Filme pesquisado
+#	E Filme selecionado
+#	Quando Clicado no botão alugar da modal
+#	E Pin inserido incorretamente 
+#	E Clicar no botão de confirmação
+#	Então Exibe mensagem de PIN inválido "Senha Incorreta"
+#
+#
+#	Cenário: Aluguel filme PIN em branco
+#	Dado o navegador esta aberto
+#	E a URL acessada é o NOW
+#	E Fechado o Walkthrough
+#	E Login efetuado
+#	E Filme pesquisado
+#	E Filme selecionado
+#	Quando Clicado no botão alugar da modal
+#	E Clicar no botão de confirmação
+#	Então Exibe mensagem de PIN inválido "Senha Incorreta"
---
+++ DojoBDD2017/AluguelSteps.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using OpenQA.Selenium.Interactions;
+
+namespace DojoBDD2017
+{
+    [Binding]
+    public class AluguelSteps
+    {
+        public IWebDriver webdriver; 
+
+        [BeforeScenario]
+        public void Initialize()
+        {
+            webdriver = new ChromeDriver();
+        }
+
+        [AfterScenario]
+        public void CleanUp()
+        {
+            webdriver.Close();
+            webdriver.Dispose();
+        }
+
+        [Given(@"que esteja na página inicial")]
+        public void DadoQueEstejaNaPaginaInicial()
+        {
+
+            webdriver.Navigate().GoToUrl("http://webportal.nowonline.com.br");
+                       
+        }
+     
+        [Given(@"Fechado o Walkthrough")]
+        public void DadoFechadoOWalkthrough()
+        {
+            WebDriverWait wait = new WebDriverWait(webdriver, new TimeSpan(0, 0, 5));
+            var closeModalButton = wait.Until<IWebElement>(ExpectedConditions.ElementIsVisible(By.ClassName("btn-close-modal")));
+            closeModalButton.Click();
+
+        }
+        
+        [Given(@"Login efetuado")]
+        public void DadoLoginEfetuado()
+        {
+            WebDriverWait wait = new WebDriverWait(webdriver, new TimeSpan(0, 0, 5));
+            
+            string currentHandle = webdriver.CurrentWindowHandle;
+            ReadOnlyCollection<string> originalHandles = webdriver.WindowHandles;
+
+            var buttonLogin = wait.Until<IWebElement>(ExpectedConditions.ElementIsVisible(By.ClassName("btn-login")));            
+
+            PopupWindowFinder finder = new PopupWindowFinder(webdriver);
+            string popupWindowHandle = finder.Click(buttonLogin);
+           
+         
+            webdriver.SwitchTo().Window(popupWindowHandle);
+
+            var inputPassword = wait.Until<IWebElement>(ExpectedConditions.ElementIsVisible(By.Name("passwordHint")));
+            inputPassword.Click();
+
+            var password = webdriver.FindElement(By.Id("password"));
+            password.SendKeys("nowmulti1");
+
+
+            var inputUserName = wait.Until<IWebElement>(ExpectedConditions.ElementIsVisible(By.Name("username")));
+            inputUserName.SendKeys("nowmultiscreen1");
+            
+            webdriver.FindElement(By.Name("_submit")).Click();
+
+            webdriver.SwitchTo().Window(currentHandle);                                  
+        }
+        
+        [Given(@"Filme pesquisado")]
+        public void DadoFilmePesquisado()
+        {
+            Thread.Sleep(3000);
+            IWebElement poster = webdriver.FindElement(By.ClassName("carousel-item"));
+            Actions mouseover = new Actions(webdriver);
+            mouseover.MoveToElement(poster).Perform();
+            Thread.Sleep(5000);
+            IWebElement detalhes = webdriver.FindElement(By.XPath("//*[@href='/filme/meus-15-anos-pre-lancamento/92200']"));
+            Thread.Sleep(3000);
+        }
+        
+        [Given(@"Filme selecionado")]
+        public void DadoFilmeSelecionado()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Clicado no botão alugar da modal")]
+        public void QuandoClicadoNoBotaoAlugarDaModal()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Pin inserido corretamente")]
+        public void QuandoPinInseridoCorretamente()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Clicar no botão de confirmação")]
+        public void QuandoClicarNoBotaoDeConfirmacao()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Pin inserido incorretamente")]
+        public void QuandoPinInseridoIncorretamente()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Usuário é encaminhado à tela de reprodução\.")]
+        public void EntaoUsuarioEEncaminhadoATelaDeReproducao_()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Exibe mensagem de PIN inválido ""(.*)""")]
+        public void EntaoExibeMensagemDePINInvalido(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
